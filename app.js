df48bd31cec7c8c4923f2ae554879fc547484fe7
ThePursuitofCode <thepursuitofcode@gmail.com>
2022-06-24 20:45:09 -0600

    styling the active form for submitting a book to the library for the odin project.

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ function addBookToLibrary() {
 
 }
 
+
+// When "add book btn" is pressed bring up a form and upon submitting take the title, author, pages.
+//  And if they have read it and create a bookCard.
+// id = "notCompletedBtn" or id ="completedBtn" 3 paragraphs, 1btn with id and class="btn" and 1btn with class="btn"
+const addBookBtn = document.querySelector('#addBookBtn');
+
+
+function createForm(){
+
+}
+
